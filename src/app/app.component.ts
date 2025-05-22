03d2ee5a1fbc0d37f64ade4c9530b25d44d80a5d
Moises García <utti222026@utvco.edu.mx>
2025-05-22 09:28:11 -0600

    union de 2 rama

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'proyecto_4';
+
+  constructor(){
+    console.log("Funciones de prueba");
+    console.warn("Advertencia");
+    console.log("Prueba de conflictos");
+    
+    
+  }
 }
