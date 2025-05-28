5d2d4428cc0cbf1a53df7cd53f67490cd34cb7b3
Moises García <utti222026@utvco.edu.mx>
2025-05-28 09:37:55 -0600

    creacion_componente_usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UsuariosComponent } from './usuarios/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
+  { path: 'usuarios', component: UsuariosComponent },
+];
