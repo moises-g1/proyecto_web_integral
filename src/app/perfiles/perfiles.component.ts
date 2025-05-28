6261fdafc68f7ff7176c19cfe3c491265ba399df
oshersa-170701 <oshersa170701@gmail.com>
2025-05-28 09:24:19 -0600

    Creacion del componente Perfiles

---
+++ src/app/perfiles/perfiles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfiles',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfiles.component.html',
+  styleUrl: './perfiles.component.css'
+})
+export class PerfilesComponent {
+
+}
